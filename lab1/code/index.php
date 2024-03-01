c394222310ece0706321c93d74f802d166f7db56
IgorRunny <ibolshacov@yandex.ru>
2024-03-01 19:49:55 +0200

    added task 3 solution(lab2)

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -1,17 +1,13 @@
 <?php
-/* task1 */
+// task1
 $very_bad_unclear_name = "15 chicken wings";
-
-
 
 $order = & $very_bad_unclear_name;
 $order .= ", but have some new letters";
 
-
 echo "\nYour order is: $very_bad_unclear_name.\n";
 
-/* task2 */
-
+// task2
 $AnyInt = 52;
 echo $AnyInt;
 echo "\n";
@@ -27,3 +23,10 @@ $LastMonth = 1187.23;
 $ThisMonth = 1089.98;
 echo "last month I spent more on: ". $LastMonth - $ThisMonth;
 echo "\n";
+
+// task3
+$NumLanguages = 4;
+$Month = 11;
+$Days = $Month * 16;
+$DaysPerLanguage = $Days / $NumLanguages;
+echo "On average each language was studied: $DaysPerLanguage days\n";
