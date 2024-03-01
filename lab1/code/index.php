04942133be8144ca52877a9b50fa6fd5c1795cdf
IgorRunny <ibolshacov@yandex.ru>
2024-03-01 20:53:14 +0200

    added task 6 solution(lab2)

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -44,3 +44,76 @@ $Answer -= 2;
 $Answer /= 2;
 $Answer -= $MyNum;
 echo"$Answer \n";
+
+// task6
+$a=10;
+$b=3;
+echo $a % $b;
+echo "\n";
+
+if(0 === $a % $b){
+    echo "divide\n";
+}else{
+    echo "not divide\n";
+}
+
+$st = pow(2, 10);
+$qr = sqrt(245);
+$t = [4, 2, 5, 19, 13, 0, 10];
+$Character = 0;
+foreach($t as $value){
+    $Character += pow($value, 2);
+}
+$Character = sqrt($Character);
+
+$Number0 = round(sqrt(349), 0);
+$Number1 = round(sqrt(349), 1);
+$Number2 = round(sqrt(349), 2);
+
+$NumC = ceil(sqrt(587));
+$NumF = floor(sqrt(587));
+$Arr = [
+    "ceil" => $NumC,
+    "floor" => $NumF,
+];
+
+$Array = [4, -2, 5, 19, -130, 0, 10];
+$Min = min($Array);
+$Max = max($Array);
+
+echo rand(1, 100);
+echo "\n";
+$ArrayOfRandNum = [];
+for ($i = 0; $i < 10; $i++){
+    $ArrayOfRandNum[$i] = rand(1, 100);
+}
+
+$a = 231;
+$b = 233;
+$Abs = abs($a - $b);
+
+$ArrayOfNum = [1, 2, -1, -2, 3, -3];
+foreach ($ArrayOfNum as $value){
+    $value = abs($value);
+}
+
+$ControlNum = rand(1, 1000);
+$i = 1;
+$Arr = [];
+while ($i <= $ControlNum){
+    if(0 === $ControlNum % $i){
+       $Arr[] = $i;
+    }
+    $i++;
+}
+
+$Arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$Ans = 0;
+$Counter = 0;
+foreach($Arr1 as $value){
+    if($Ans > 10){
+        break;
+    }
+    $Ans += $value;
+    $Counter++;
+}
