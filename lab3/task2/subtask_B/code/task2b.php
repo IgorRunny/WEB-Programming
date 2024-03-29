3269ed3524f438891c1f269265f2cf73d86bc7a4
IgorRunny <ibolshacov@yandex.ru>
2024-03-29 23:02:01 +0200

    add subtask 'b'

---
+++ lab3/task2/subtask_B/code/task2b.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset="UTF-8">
+    <title> Task_2 </title>
+</head>
+
+<body>
+    <form method="post" action="session.php">
+        <label> Print your surname </label>
+        <input type = "text" name = "surname" required> <br>
+
+        <label> Print your name </label>
+        <input type = "text" name = "name" required> <br>
+
+        <label> Print your age </label>
+        <input type = "text" name = "age" required> <br>
+
+        <input type = "submit" value = "confirm">
+    </form>
+</body>
+</html>
+
